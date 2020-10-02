42025009e8fe2794e5f1a763c7e7091bee98c9d9
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-10-02 18:14:45 +0500

    Добавит mapDispatchToProps для BooksList

--- src/components/book-list/book-list.jsx
+++ src/components/book-list/book-list.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import BookListItem from "../book-list-item/book-list-item.jsx";
 import {connect} from "react-redux";
+import withBookstoreService from "../hoc/with-bookstore-service.js";
+import {booksLoaded} from "../../actions/index.js";
+import {bindActionCreators} from "redux";
+import compose from "../../utils/compose.js";
 import "./book-list.css";
 
 class BookList extends React.Component {
+  componentDidMount() {
+    const {bookstoreService} = this.props;
+    const data = bookstoreService.getBooks();
+    this.props.booksLoaded(data);
+  }
+
   render() {
     const {books} = this.props;
 
@@ -29,4 +39,11 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({booksLoaded}, dispatch);
+};
+
+export default compose(
+    withBookstoreService(),
+    connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
