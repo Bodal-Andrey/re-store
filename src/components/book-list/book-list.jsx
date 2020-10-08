2e1bb31d0bc96dfdb5fc7524f5281f680ec782d4
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-10-08 18:12:16 +0500

    Внесёт некоторые изменения

--- src/components/book-list/book-list.jsx
+++ src/components/book-list/book-list.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import BookListItem from "../book-list-item/book-list-item.jsx";
+import Spinner from "../spinner/spinner.jsx";
 import {connect} from "react-redux";
-import withBookstoreService from "../hoc/with-bookstore-service.js";
+import {withBookstoreService} from "../hoc/with-bookstore-service.js";
 import {booksLoaded} from "../../actions/index.js";
 import {bindActionCreators} from "redux";
 import compose from "../../utils/compose.js";
@@ -9,13 +10,19 @@ import "./book-list.css";
 
 class BookList extends React.Component {
   componentDidMount() {
-    const {bookstoreService} = this.props;
-    const data = bookstoreService.getBooks();
-    this.props.booksLoaded(data);
+    const {bookstoreService, booksLoaded} = this.props;
+    bookstoreService.getBooks()
+    .then((data) => {
+      booksLoaded(data);
+    });
   }
 
   render() {
-    const {books} = this.props;
+    const {books, loading} = this.props;
+
+    if (loading) {
+      return <Spinner />;
+    }
 
     return (
       <ul className="book-list">
@@ -35,7 +42,8 @@ class BookList extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    books: state.books
+    books: state.books,
+    loading: state.loading,
   };
 };
 
